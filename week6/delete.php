a9917c4c0c6f274b6ca27c7aacafd5dcc4e10e15
xxjeanxx80 <xxjeanxx80@gmail.com>
2025-10-26 10:55:33 +0700

    â

--- week6/delete.php
+++ week6/delete.php
@@ -1,14 +1,31 @@
 <?php
 include "connection.php";
 
-$id=$_GET["id"];
-mysqli_query($link,"delete from table1 where id=$id");
-header("location.index.php");
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Xử lý khi user nhấn Yes hoặc No
+    if (isset($_POST['confirm'])) {
+        if ($_POST['confirm'] === 'Yes' && isset($_POST['id'])) {
+            $id = intval($_POST['id']);
+            mysqli_query($link, "DELETE FROM table1 WHERE id=$id");
+        }
+        // Quay lại trang index bất kể Yes hay No
+        header("Location: index.php");
+        exit();
+    }
+} else {
+    // Hiển thị form xác nhận
+    if (!isset($_GET['id'])) {
+        header("Location: index.php");
+        exit();
+    }
+    $id = intval($_GET['id']);
+    ?>
+    <h3>Are you sure to delete this item with ID = <?php echo $id; ?>?</h3>
+    <form method="post">
+        <input type="hidden" name="id" value="<?php echo $id; ?>">
+        <button type="submit" name="confirm" value="Yes">Yes</button>
+        <button type="submit" name="confirm" value="No">No</button>
+    </form>
+    <?php
+}
 ?>
-
-<script type="text/javascript">
- window.location="index.php";
-    </script>
-
-
-
